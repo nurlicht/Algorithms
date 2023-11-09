e59f57b6b7e2bf044955f99c2ccdc7c559e6fa89
nurlicht <saman@gatech.edu>
2023-11-09 13:40:50 +0100

    Add code and documentation

---
+++ README.md
@@ -0,0 +1,72 @@
+# Graph Traversal
+
+### Algorithm Features
+- Single-pass
+- Non-recursive
+- Simple character-scan of xml-string
+    - No use of find, indexOf ...
+
+### Test Case
+  - XML-parsing
+
+### Execution
+```
+node ./xml-parser.js
+```
+
+### Console Output (demo)
+```
+Input (xmlString):
+    <Item_0>
+      <Item_0_0 key1="value1" key2="value2">
+        4
+      </Item_0_0>
+      <Item_0_1>
+        <Item_0_1_0>
+          Hello World!
+        </Item_0_1_0>
+      </Item_0_1>
+    </Item_0>
+
+(stringified) Output (xmlObject): {
+  "name": "Item_0",
+  "children": [
+    {
+      "name": "Item_0_0",
+      "attributes": {
+        "key1": "value1",
+        "key2": "value2"
+      },
+      "value": "4"
+    },
+    {
+      "name": "Item_0_1",
+      "children": [
+        {
+          "name": "Item_0_1_0",
+          "value": "Hello World!"
+        }
+      ]
+    }
+  ]
+}
+```
+
+### Application with other clients
+Simply call the following static method (as in the demo-class):
+```
+XmlParser.parse(xmlString)
+``` 
+
+### Implementation
+- Data Model
+  - XmlTag: Name, indices, attributes ... of an XML-tag
+  - XmlNode: Graph-based model for values, children ... of an XML-node
++ Logic
+  1. Detect tags
+  2. Scan tags
+        - Generate new nodes with new start tags (<>)
+        - Keep track of the previous tag/node
+        - Associate a new node with its parent
+        - Assign values to leaves upon detection of an end tag (</>)
+        
---
+++ xml-parser.js
@@ -0,0 +1,254 @@
+class Utilities {
+  static hasValue(x) {
+    return x !== null && x !== undefined;
+  }
+
+  static withDefault(x, xDefault) {
+    return Utilities.hasValue(x) ? x : xDefault;
+  }
+}
+
+class XmlTag {
+  tagString;
+  firstIndex;
+  secondIndex;
+  attributes;
+
+  constructor(
+    tagString,
+    firstIndex,
+    secondIndex
+  ) {
+    this.tagString = tagString;
+    this.firstIndex = firstIndex;
+    this.secondIndex = secondIndex;
+    this.attributes = this.getAtributes();
+  }
+  
+  static getNext(xmlString, searchStartIndex) {
+    const xmlStringLength = xmlString.length;
+    let firstIndex = -1;
+    let secondIndex = -1;
+    let index = searchStartIndex;
+    while (secondIndex < 0 && index < xmlStringLength) {
+      const c = xmlString.charAt(index);
+      if (c === '<') {
+        if (firstIndex > 0) {
+          throw new Error('Unexpected < was encountered.');
+        }
+        firstIndex = index;
+      } else if (c === '>') {
+        if (firstIndex < 0) {
+          throw new Error('Unexpected > was encountered.');
+        }
+        secondIndex = index;
+      }
+      ++index;
+    }
+    if (secondIndex < 0) {
+      return null;
+    }
+    return new XmlTag(
+      xmlString.substring(firstIndex, secondIndex + 1),
+      firstIndex,
+      secondIndex
+    );
+  }
+
+  getAtributes() {
+    const keyValues = {};
+    this.getNameAndAtributes()
+      .slice(1)
+      .map((x) => x.trim().split('='))
+      .forEach((x) => keyValues[x[0].trim()] = x[1].trim().replaceAll('"', ''))
+    ;
+    return keyValues;
+  }
+
+  getName() {
+    return this.getNameAndAtributes()[0];
+  }
+
+  getNameAndAtributes() {
+    if (!this.isValid()) {
+      throw new Error('Invalid Tag was encountered.');
+    }
+    return this.tagString.substring(this.isEndTag() ? 2 : 1, this.tagString.length - 1).split(' ');
+  }
+
+  isEndTag() {
+    if (!this.isValid()) {
+      throw new Error('Invalid Tag was encountered.');
+    }
+    return this.tagString.startsWith('</');
+  }
+
+  isValid() {
+    return (
+      Utilities.hasValue(this.tagString) &&
+      this.tagString.startsWith('<') &&
+      this.tagString.charAt(this.tagString.length - 1) === '>' &&
+      Number.isInteger(this.firstIndex) &&
+      Number.isInteger(this.secondIndex)
+    );
+  }
+}
+
+class XmlNode {
+  parent;
+  name;
+  attributes;
+  children;
+  value;
+
+  constructor(parent, name, attributes, children, value) {
+    this.parent = parent;
+    this.name = name;
+    this.attributes = attributes;
+    this.children = Utilities.withDefault(children, []);
+    this.value = value;
+    if (Utilities.hasValue(parent)) {
+      parent.children.push(this);
+    }
+  }
+  
+  static create(parent, name, attributes) {
+    return new XmlNode(parent, name, attributes, [], null);
+  }
+  
+  isLeaf() {
+    return this.children.length === 0;
+  }
+
+  setValue(value) {
+    const hasValue = Utilities.hasValue(value);
+    if (hasValue && !this.isLeaf()) {
+      throw new Error('Value cannot be assigned to a non-leaf node.');
+    }
+    this.value = hasValue ? value.toString().replaceAll('\\n', '').trim() : null;
+  }
+}
+
+// Main class to be called by clients (see the demo below)
+class XmlParser {
+  static parse(xmlString) {
+    if (typeof xmlString !== 'string') {
+      throw new Error('Invalid XML-string was encountered.');
+    }
+    const tags = XmlParser.getTags(xmlString);
+    const xmlNodeRoot = XmlNode.create(null, null);
+    XmlParser.addValuesAndChildren(xmlString, tags, xmlNodeRoot);
+    const root = xmlNodeRoot.children[0];
+    return root;
+  }
+
+  static getTags(xmlString) {
+    const xmlStringLength = xmlString.length;
+    const tags = [];
+    let index = 0;
+    while (index < xmlStringLength) {
+      const tag = XmlTag.getNext(xmlString, index);
+      if (Utilities.hasValue(tag)) {
+        tags.push(tag);
+        index = tag.secondIndex + 1;
+      } else {
+        index = xmlStringLength;
+      }
+    }
+    return tags;
+  }
+
+  static addValuesAndChildren(xmlString, tags, parent) {
+    let lastNode = parent;
+    let previousTag = null;
+    for (const tag of tags) {
+      if (!tag.isEndTag()) { // <TAG>
+        const parent = (
+          Utilities.hasValue(previousTag) &&
+          previousTag.isEndTag() &&
+          Utilities.hasValue(lastNode)
+          ) ? lastNode.parent : lastNode;
+        lastNode = XmlNode.create(parent, tag.getName(), tag.getAtributes());
+      } else { // </TAG>
+        if (!Utilities.hasValue(previousTag)) {
+          throw new Error('Unexpected </TAG> was encountered.');
+        }
+        const isLeaf = (
+          !previousTag.isEndTag() &&
+          previousTag.getName() === tag.getName()
+        );
+        if (isLeaf) {
+          const value = xmlString.substring(
+            previousTag.secondIndex + 1,
+            tag.firstIndex
+          );
+          lastNode.setValue(value);
+        } 
+      }
+      previousTag = tag;
+    }
+  }
+}
+
+
+// ***************************  For demo only ***************************
+class XmlSynthesizer {
+  static create() {
+    return `
+    <Item_0>
+      <Item_0_0 key1="value1" key2="value2">
+        4
+      </Item_0_0>
+      <Item_0_1>
+        <Item_0_1_0>
+          Hello World!
+        </Item_0_1_0>
+      </Item_0_1>
+    </Item_0>
+    `;
+  }
+}
+
+
+class XmlNodeUtil {
+  static removeParentsAndNulls(node) {
+    if (!Utilities.hasValue(node)) {
+      return;
+    }
+    node.parent = undefined;
+    for (const child of node.children) {
+      XmlNodeUtil.removeParentsAndNulls(child);
+    }
+    if (node.children.length === 0) {
+      node.children = undefined;
+    }
+    if (!Utilities.hasValue(node.value)) {
+      node.value = undefined;
+    }
+    if (!Utilities.hasValue(node.attributes) || Object.keys(node.attributes).length === 0) {
+      node.attributes = undefined;
+    }
+  }
+} 
+
+class Demo {
+  static parseSimulatedXmlString() {
+    const xmlString = XmlSynthesizer.create();
+    console.log('Input (xmlString):', xmlString);
+    console.assert(typeof xmlString === 'string', 'Input is not a string-type (rendered version of) xml.');
+    
+    const xmlObject = XmlParser.parse(xmlString);
+    XmlNodeUtil.removeParentsAndNulls(xmlObject); 
+    console.log('(stringified) Output (xmlObject):', Demo.stringify(xmlObject));
+    console.assert(xmlObject.children[0].value === '4', "The '4' leaf was not parsed correctly.");
+    console.assert(xmlObject.children[1].children[0].value === 'Hello World!', "The 'Hello World!' leaf was not parsed correctly.");
+
+    return xmlObject;
+  }
+
+  static stringify(x) {
+    return JSON.stringify(x, null, '  ');
+  }
+}
+
+Demo.parseSimulatedXmlString();
